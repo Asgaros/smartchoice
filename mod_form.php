e6d274e737d1e1e36e5874d115e1bf4e92d2d443
Thomas <developer@thomasbelser.net>
2016-10-26 17:14:53 +0200

    Changed required version and added GPL license
    Changed required version and added GPL license.

--- mod_form.php
+++ mod_form.php
@@ -1,4 +1,19 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.');
 }
@@ -54,7 +69,7 @@ class mod_smartchoice_mod_form extends moodleform_mod {
         $mform->disabledIf('timeopen', 'timerestrict');
         $mform->addElement('date_time_selector', 'timeclose', get_string("choiceclose", "smartchoice"));
         $mform->disabledIf('timeclose', 'timerestrict');
-        
+
         $this->standard_coursemodule_elements();
         $this->add_action_buttons();
     }
